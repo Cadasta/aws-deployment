3df8717f8fbd3e80403c913c3b624c18940c533d
Ian Ross <iross@cadasta.org>
2015-12-04 13:31:15 +0100

    Make ONA admin password setup work!

--- playbooks/deploy/provision/roles/ona/files/create-superuser.sh
+++ playbooks/deploy/provision/roles/ona/files/create-superuser.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PWD=$1
+export PASS=$1
 cd /opt/ona/onadata
 . /opt/ona/onadata/bin/activate
 python manage.py shell <<EOF
 from django.contrib.auth.models import User
 u, created = User.objects.get_or_create(username='admin')
-u.set_password('$PWD')
+u.set_password('$PASS')
 u.is_superuser = True
 u.is_staff = True
 u.save()
